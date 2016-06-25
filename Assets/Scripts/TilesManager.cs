411c722210bf2182660f6d1b1208c575c4e10491
ABAmvrosov <abamvrosov@gmail.com>
2016-06-25 20:30:31 +0300

    UnionFind DT

--- Assets/Scripts/TilesManager.cs
+++ Assets/Scripts/TilesManager.cs
@@ -9,9 +9,41 @@ public class TilesManager : MonoBehaviour {
     private Graph<Tile> _graph;
     private Dictionary<string, Tile> _tilesByName;
     [SerializeField] private Transform[] _playersTransform;
-    [SerializeField] private Tile[] _winTilePlayer1;
-    [SerializeField] private Tile[] _winTilePlayer2;
-    
+	[SerializeField] private GameObject[] _winTileContainers;
+	private List<Tile[]> _winTiles;
+
+	private void Awake() {
+		_tilesByName = new Dictionary<string, Tile>();
+		InitGraph();
+		//Debug.Log(_graph.ToString());
+	}
+
+	private void InitGraph() {
+		var tiles = GetComponentsInChildren<Tile>();
+		_graph = new Graph<Tile>();
+		foreach (var tile in tiles) {
+			tile.name = tile.ToString(); // for editor comfort
+			_tilesByName.Add(tile.name, tile);
+			_graph.AddVertice(tile);
+			ProceedDirection(tile, Vector2.down);
+			ProceedDirection(tile, Vector2.right);
+		}
+	}
+
+	private void ProceedDirection(Tile tile, Vector2 direction) {
+		int tmp = tile.gameObject.layer;
+		tile.gameObject.layer = 2;
+		RaycastHit2D[] hits = Physics2D.RaycastAll(new Vector2(tile.X, tile.Y), direction, Tile.Dimension);
+		foreach (var hit in hits) {
+			if (hit.collider != null && hit.collider.CompareTag("SimpleGround")) {
+				Tile dstTile = hit.collider.GetComponent<Tile>();
+				_graph.AddVertice(dstTile);
+				_graph.AddEdge(tile, dstTile);
+			}
+		}        
+		tile.gameObject.layer = tmp;
+	}    
+
     public List<Tile> AdjacentTiles(Tile origin) {
         return _graph.GetAdjacentVertices(origin);
     }
@@ -29,22 +61,10 @@ public class TilesManager : MonoBehaviour {
             deletedEdges[counter] = _graph.DeleteVerticeAndStoreAdjVertices(origins[counter]);
             counter++;
         }
-        bool canPlayer1Path = false;
-        bool canPlayer2Path = false;
-        Paths<Tile> paths1 = new Paths<Tile>(_graph, GetTile(_playersTransform[0].position));
-        Paths<Tile> paths2 = new Paths<Tile>(_graph, GetTile(_playersTransform[1].position));
-        foreach (Tile tile in _winTilePlayer1) {
-            if (paths1.hasPathTo(tile)) {
-                canPlayer1Path = true;
-                break;
-            }                
-        }
-        foreach (Tile tile in _winTilePlayer2) {
-            if (paths2.hasPathTo(tile)) {
-                canPlayer2Path = true;
-                break;
-            }
-        }
+		bool[] canPlayersPath = new bool[_playersTransform.Length];
+		for (int i = 0; i < _playersTransform.Length - 1; i++) {
+			CanPlayerPath(i, ref canPlayersPath[i]);
+		}
         deletedEdges[1].Remove(origins[0]);
         deletedEdges[0].Remove(origins[1]);
         for (int i = 0; i < origins.Length; i++) {
@@ -54,8 +74,27 @@ public class TilesManager : MonoBehaviour {
             }
         }
         _graph.AddEdge(origins[0], origins[1]);
-        return canPlayer1Path & canPlayer2Path;
+		return IsFalseInBoolArray(canPlayersPath);
     }
+
+	private bool CanPlayerPath(int playerNumber, ref bool canPlayerPath) {
+		Paths<Tile> paths = new Paths<Tile>(_graph, GetTile(_playersTransform[playerNumber].position));
+		foreach (Tile tile in _winTiles[playerNumber]) {
+			if (paths.hasPathTo(tile)) {
+				canPlayerPath = true;
+				return canPlayerPath;
+			}
+		}
+		return false;
+	}
+
+	private bool IsFalseInBoolArray(bool[] array) {
+		bool result = true;
+		foreach (bool b in array) {
+			result = result & b;
+		}
+		return !result;
+	}
 
     public Tile GetTile(Vector3 coordinates) {
         //Debug.Log("1. Coordinates before cast: " + coordinates.x + ", " + coordinates.y);
@@ -74,41 +113,4 @@ public class TilesManager : MonoBehaviour {
         tile.gameObject.SetActive(false);
         _graph.DeleteVertice(GetTile(coordinates));
     }
-
-    public void TestFunc() {
-        Paths<Tile> paths = new Paths<Tile>(_graph, GetTile(new Vector3(3.0f, -6.0f)));
-        Debug.Log(paths.hasPathTo(GetTile(new Vector3(-1.0f, 4.0f))));
-    }
-
-    private void Awake() {
-        _tilesByName = new Dictionary<string, Tile>();
-        InitGraph();
-        //Debug.Log(_graph.ToString());
-    }
-
-    private void InitGraph() {
-        var tiles = GetComponentsInChildren<Tile>();
-        _graph = new Graph<Tile>();
-        foreach (var tile in tiles) {
-            tile.name = tile.ToString(); // for editor comfort
-            _tilesByName.Add(tile.name, tile);
-            _graph.AddVertice(tile);
-            ProceedDirection(tile, Vector2.down);
-            ProceedDirection(tile, Vector2.right);
-        }
-    }
-
-    private void ProceedDirection(Tile tile, Vector2 direction) {
-        int tmp = tile.gameObject.layer;
-        tile.gameObject.layer = 2;
-        RaycastHit2D[] hits = Physics2D.RaycastAll(new Vector2(tile.X, tile.Y), direction, Tile.Dimension);
-        foreach (var hit in hits) {
-            if (hit.collider != null && hit.collider.CompareTag("SimpleGround")) {
-                Tile dstTile = hit.collider.GetComponent<Tile>();
-                _graph.AddVertice(dstTile);
-                _graph.AddEdge(tile, dstTile);
-            }
-        }        
-        tile.gameObject.layer = tmp;
-    }    
 }
---
+++ Assets/Scripts/UnionFind.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System;
+
+public class UnionFind {
+	private int[] _parent;
+	private int[] _size;
+	private int _count;
+
+	public UnionFind(int N) {
+		_count = N;
+		_parent = new int[N];
+		_size = new int[N];
+		for (int i = 0; i < N; i++) {
+			_parent[i] = i;
+			_size[i] = 1;
+		}
+	}
+
+	public int Count() {
+		return _count;
+	}
+
+	public int Find(int p) {
+		Validate (p);
+		while (p != _parent[p])
+			p = _parent[p];
+		return p;
+	}
+
+	private void Validate(int p) {
+		int n = _parent.Length;
+		if (p < 0 || p >= n) {
+			throw new IndexOutOfRangeException("index " + p + " is not between 0 and " + (n - 1));
+		}
+	}
+
+	public Boolean Connected(int p, int q) {
+		return Find(p) == Find(q);
+	}
+
+	public void Union(int p, int q) {
+		int rootP = Find(p);
+		int rootQ = Find(q);
+		if (rootP == rootQ)
+			return;
+		if (_size[rootP] < _size[rootQ])
+		{
+			_parent[rootP] = rootQ;
+			_size[rootQ] += _size[rootP];
+		}
+		else {
+			_parent[rootQ] = rootP;
+			_size[rootP] += _size[rootQ];
+		}
+		_count--;
+	}
+}
+
+
+
+
+
+
+
+
+
